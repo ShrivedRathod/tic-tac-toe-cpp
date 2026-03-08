9b326a99354a83957285554c4ccf5505d0790023
ShrivedRathod <shrivedrathod28@gmail.com>
2026-03-08 23:11:05 +0530

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char board [3][3] ={
+        {' ',' ', ' ',},
+        {' ', ' ', ' ',},
+        {' ', ' ', ' ',}
+    };
+    
+    const char playerX= 'X';
+    const char playerO= 'O';
+    char currentPlayer= playerX; 
+
+    int r = -1;
+    int c = -1;
+    char winner = ' ';// this means there are no winnner but after every turn we gonna check the winners if theres winner then we going to assing either x or o
+
+
+    for(int i = 0 ; i < 9; i++){
+    //  printing the game board
+    cout<< "   |   |   " <<endl;
+    cout<< " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+    cout<< "___|___|___"<<endl;
+    cout<< "   |   |   "<<endl;
+    cout<< " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+    cout<< "___|___|___"<<endl;
+    cout<< "   |   |   "<<endl;
+    cout<< " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+    cout<< "   |   |   "<<endl;
+
+    if( winner != ' '){
+        break;
+        
+    }
+
+    //get player input
+    cout<< "current player is"<< currentPlayer << endl;
+    
+
+    while(true){
+        //just because it is overwriting 
+    cout<<"enter r and c from 0-2 for row and coloumn";
+    cin>> r >> c;//integers
+    if( r < 0 || r > 2 || c  < 0 || c > 2){
+        cout<< "invallid input try again" <<  endl;
+
+    }
+    else if( board[r][c] != ' ')//not empty
+    {
+        cout << " tile is full"<< endl;
+
+    }// above are the two issues
+    else{
+        break;
+
+    }
+    r = -1;
+    c = -1; // reset
+    }
+
+    board[r][c] = currentPlayer;
+    currentPlayer = ( currentPlayer == playerX) ? playerO : playerX;
+        //if currentplayer is equal to playerx then assign playerO to current player Otherwise assign playerX to currentplayer
+        // ternary operator ? to switdch between two players.
+        
+         //check winners
+         //rows- horizontal
+        for(int r = 0; r < 3; r++){
+            if(board[r][0] != ' ' && board[r][0]== board[r][1] && board[r][1]== board[r][2]){// if everything matches then we have winner 
+                winner = board[r][0];
+                break;
+                
+
+            }
+        }
+        //columns - vertical
+        for(int c = 0 ; c < 3 ; c ++){
+            if(board[0][c] != ' ' && board[0][c]== board[1][c] && board[1][c]== board[2][c]){
+                winner = board[0][c];
+                break;
+
+            }
+        }
+        //below are for the diagonals
+        if ( board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            winner = board[0][0];
+            break;
+
+        }
+        else if( board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1]== board[2][0]){
+             winner =  board[0][2];
+        }
+       
+    }
+
+
+    if(winner != ' '){
+    cout<< " player" << winner << "is the winner" << endl;
+    }
+    else{
+        cout<< "tie" << endl;
+        
+    }
+}
+
+   
